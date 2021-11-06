510c58f260a44074dc46fd8d3f07d5ac7bec76cd
Tristan Fumiere <tristan_fumiere@ens.univ-artois.fr>
2021-11-06 04:00:02 +0100

    Adding JSON file

--- src/lib/script.js
+++ src/lib/script.js
@@ -2,37 +2,23 @@
  var aQuiLeTour = 1;
  var plateau = new Array(null,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39);
  var nomCases = new Array(null,"Case départ","Boulevard de Belleville","Caisse de communauté","Rue Lecourbe","Impôts sur le revenu","Gare Montparnasse","Rue de Vaugirard","Chance","Rue de Courcelles","Avenue de la République","Prison","Boulevard de la Villette","Compagnie de distribution d'électricité","Avenue de Neuilly","Rue de Paradis","Gare de Lyon","Avenue Mozart","Caisse de communauté","Boulevard Saint-Michel","Place Pigalle","Parc Gratuit","Avenue Matignon","Chance","Boulevard Malesherbes","Avenue Henri-Martin","Gare du Nord","Faubourg Saint-Honoré","Place de la Bourse","Compagnie de distribution des eaux","Rue la Fayette","Allez en prison","Avenue de Breuteuil","Avenue Foch","Caisse de communauté","Boulevard des Capucines","Gare Saint-Lazare","Chance","Avenue des Champs-Elysées","Taxe de luxe","Rue de la Paix");
- var prixPropriétés = new Array(null,null,6000,null,6000,null,20000,10000,null,10000,10000,null,14000,15000,14000,16000,20000,18000,null,18000,20000,null,22000,null,22000,24000,20000,26000,26000,15000,28000,null,30000,30000,null,32000,20000,null,35000,null,40000);
  var maisons = new Array(null,null,0,null,0,null,0,0,null,0,0,null,0,0,0,0,0,0,null,0,0,null,0,null,0,0,0,0,0,0,0,null,0,0,null,0,0,null,0,null,0);
- var loyer = new Array();
-  loyer[2]= new Array(200,400,1000,3000,9000,16000,25000);//Boulevard de Belleville
-  loyer[4]= new Array(400,800,2000,6000,18000,32000,45000);//Rue Lecourbe
-  loyer[6]= new Array(2500,5000,10000,20000);//Gare Montparnasse
-  loyer[7]= new Array(600,1200,3000,9000,27000,40000,55000);//Rue de Vaugirard
-  loyer[9]= new Array(600,1200,3000,9000,27000,40000,55000);//Rue de Courcelles
-  loyer[10]= new Array(800,1600,4000,10000,30000,45000,60000);//Avenue de la République
-  loyer[12]= new Array(1000,2000,5000,15000,45000,62500,75000);//Boulevard de la Villette
-  loyer[13]= new Array(resultatTirageDe*400,resultatTirageDe*1000);//Compagnie d'électricité
-  loyer[14]= new Array(1000,2000,5000,15000,45000,62500,75000)//Avenue de Neuilly
-  loyer[15]= new Array(1200,2400,6000,18000,50000,70000,90000);//Rue de Paradie
-  loyer[16]= new Array(2500,5000,10000,20000);//Gare de Lyon
-  loyer[17]= new Array(1400,2800,7000,20000,55000,75000,95000);//Avenue Mozart
-  loyer[19]= new Array(1400,2800,7000,20000,55000,75000,95000);//Boulevard Saint-Michel
-  loyer[20]= new Array(1600,3200,8000,22000,60000,80000,100000);//Place Pigalle
-  loyer[22]= new Array(1800,3600,9000,25000,70000,87500,105000);//Avenue Matignon
-  loyer[24]= new Array(1800,3600,9000,25000,70000,87500,105000);//Boulevard Malesherbes
-  loyer[25]= new Array(2000,4000,10000,30000,75000,92500,110000);//Avenue Henri-Martin
-  loyer[26]= new Array(2500,5000,10000,20000);//Gare du Nord
-  loyer[27]= new Array(2200,4400,11000,33000,80000,97500,115000);//Faubourg Saint-Honoré
-  loyer[28]= new Array(2200,4400,11000,33000,80000,97500,115000);//Place de la Bourse
-  loyer[29]= new Array(resultatTirageDe*400,resultatTirageDe*1000);//Compagnie des eaux
-  loyer[30]= new Array(2400,4800,12000,36000,85000,102500,120000);//Rue la Fayette
-  loyer[32]= new Array(2600,5200,13000,39000,90000,110000,127500);//Avenue de Breuteuil
-  loyer[33]= new Array(2600,5200,13000,39000,90000,110000,127500);//Avenue Foch
-  loyer[35]= new Array(2800,5600,15000,45000,100000,120000,140000);//Boulevard des Capucines
-  loyer[36]= new Array(2500,5000,10000,20000);//Gare Saint-Lazare
-  loyer[38]= new Array(3500,7000,17500,50000,11000,130000,150000);//Avenue des Champs-Elysées
-  loyer[40]= new Array(5000,10000,20000,60000,140000,170000,200000);//Rue de la Paix
+ 
+var requestURL = 'src/lib/stockage.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  var prop = request.response;
+  console.log(prop);
+}
+
+
+
+
+
   var communaute = new Array(null,"Vous êtes libéré de prison. Cette carte peut être conservée.","C'est votre anniversaire : Chaque joueurs doit vous donner 1000 Francs.","Erreur de la Banque en votre faveur. Recevez 20000 Francs","Allez en prison. Avancez tout droit en prison. Ne passez pas par la case départ","Recevez votre intérêt sur l'emprunt à 7% : 2500 Francs","Vous héritez 10000 Francs","Payez une amende de 1000 Francs ou tirer une carte chance","Les contributions vous remboursent la somme de 2000 Francs","Payez votre Police d'Assurance s'élevant à 5000 Francs","La vente de votre stock vous rapporte 5000 Francs","Retournez à Belleville","Vous avez gagné le deuxième prix de beauté. Recevez 1000 Francs","Placez vous sur la case départ","Recevez votre revenu annuel 10000 Francs","Payez la note du Médecin 5000 Francs","Payez à l'Hôpital 10000 Francs");
   var chance = new Array(null,"Allez en prison. Avancez tout droit en prison. Ne passez pas par la case départ","Faites des réparations dans toutes vos maisons. Versez pour chaque maison 2500 Francs. Versez pour chaque hôtel 10000 Francs","Votre immeuble et votre prêt vous rapportent. Vous touchez 15000 Francs","Avancez jusqu'à la case départ","Reculez de trois cases","Rendez vous Rue de La Paix","Allez à la gare de Lyon","Vous êtes libéré de prison. Cette carte peut être conservée","Amende pour excès de vitesse 1500 Francs","La Banque vous verse un dividende de 5000 Francs","Avancez au Boulevard de la Villette","Rendez vous à l'Avenue Henri-Martin","Payez pour frais de scolarité 15000 Francs","Vous avez gagné le prix de mots croisés. Recevez 10000 Francs","Vous êtes imposé pour les réparations de voirie à raison de : 4000 Francs par maison et 11500 Francs par hôtel","Amende pour ivresse 2000 Francs");
 
